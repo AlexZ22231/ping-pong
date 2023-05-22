a8cba9d5ac198b32ed3b266685dc83587e8b8f4e
AlexZ22231 <132229224+AlexZ22231@users.noreply.github.com>
2023-05-22 16:37:58 +0300

    Update ping-pong.py
    v 1.2 - добавлен класс-шар, создан экземпляр этого класса и размещен на сцене; добавлены автоматическое перемещение шара и смена его направления при соприкосновении с игроками

--- ping-pong.py
+++ ping-pong.py
@@ -37,16 +37,36 @@ class Player(GameSprite):
                 self.rect.y -= self.speed
             if keys[K_s] and self.rect.y < win_height -139:
                 self.rect.y += self.speed
+class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed_x, player_speed_y):
+        super().__init__(player_image, player_x, player_y)
+        self.image = transform.scale(image.load(player_image), (35,35))
+        self.speed_x = player_speed_x
+        self.speed_y = player_speed_y
+    def update(self):
+        self.rect.y += self.speed_y
+        self.rect.x += self.speed_x
+        if  self.rect.y < 4 or self.rect.y > win_height-50:
+                self.speed_y *= -1
+        if self.rect.x > win_width-20 or ball.rect.x < 0:
+            self.speed_x *= -1
+    def change_direction(self):
+        self.speed_x *= -1
+
+
+
+
+
 
 
 FPS = 60
-
 clock = time.Clock()
 
 run = True
 
 player_right = Player('rocket right.png', 650,300, 5)
 player_left = Player('rocket left.png', 30,300, 5)
+ball = Ball('ball.png', 350,250,3,3)
 while run:
     window.blit(background, (0,0))
     for e in event.get():
@@ -54,6 +74,10 @@ while run:
             run = False 
     player_right.update_r()
     player_left.update_l()
+    ball.update()
+    if sprite.collide_rect(player_left,ball) or sprite.collide_rect(player_right,ball):
+        ball.change_direction()
+    ball.reset()
     player_right.reset()
     player_left.reset()
     clock.tick(FPS)
