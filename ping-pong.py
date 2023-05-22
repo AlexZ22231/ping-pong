a74efa129f01661f8f4fd66e1d4a7f2acfb0e082
AlexZ22231 <132229224+AlexZ22231@users.noreply.github.com>
2023-05-22 16:13:00 +0300

    Update ping-pong.py
    v 1.1 - добавлен класс-спрайт, класс-игрок; создано два экземпляра класса Игрок, которые размещены на сцене и управляются клавишами

--- ping-pong.py
+++ ping-pong.py
@@ -11,6 +11,33 @@ background = transform.scale(
 image.load("background.png"), 
 (win_width, win_height)
 )
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (21,139))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__(player_image, player_x, player_y)
+        self.speed = player_speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height -139:
+            self.rect.y += self.speed
+    def update_l(self):
+            keys = key.get_pressed()
+            if keys[K_w] and self.rect.y > 5:
+                self.rect.y -= self.speed
+            if keys[K_s] and self.rect.y < win_height -139:
+                self.rect.y += self.speed
+
 
 FPS = 60
 
@@ -18,11 +45,16 @@ clock = time.Clock()
 
 run = True
 
-
+player_right = Player('rocket right.png', 650,300, 5)
+player_left = Player('rocket left.png', 30,300, 5)
 while run:
     window.blit(background, (0,0))
     for e in event.get():
         if e.type == QUIT:
             run = False 
+    player_right.update_r()
+    player_left.update_l()
+    player_right.reset()
+    player_left.reset()
     clock.tick(FPS)
     display.update()
